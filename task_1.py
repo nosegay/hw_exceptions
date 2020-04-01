2bd46c1235baf788b48bcfd10501d7ec0759d688
nosegay <github_user_2020@mail.ru>
2020-04-01 11:38:53 +0300

    Task_1 improvement

--- task_1.py
+++ task_1.py
@@ -15,29 +15,29 @@ if __name__ == '__main__':
         exit(errno.EINVAL)
 
     sign, num_1, num_2 = input_formula.split()
-    try:
-        assert sign in ('+', '-', '*', '/'), 'Операции не найдено!'
 
+    assert sign in ('+', '-', '*', '/'), 'Операции не найдено!'
+
+    try:
         num_1 = int(num_1)
         num_2 = int(num_2)
-    except AssertionError as e:
-        print(e)
-        exit(errno.EPERM)
-    except Exception as e:
+    except ValueError as e:
         print('Неверный формат входных данных: работа со строками недопустима!')
         exit(errno.EPERM)
 
-try:
-    if sign == '+':
-        print(f'Результат: {num_1 + num_2}')
-    elif sign == '*':
-        print(f'Результат: {num_1 * num_2}')
-    elif sign == '-':
-        print(f'Результат: {num_1 - num_2}')
-    elif sign == '/':
-        print(f'Результат: {num_1 / num_2}')
-except ZeroDivisionError as e:
-    print(e)
-    exit(errno.EINVAL)
-except Exception as e:
-    print(e)
+    assert (num_1 * num_2 >= 0), 'Работа с отрицательными числами не предусмотрена!'
+
+    try:
+        if sign == '+':
+            print(f'Результат: {num_1 + num_2}')
+        elif sign == '*':
+            print(f'Результат: {num_1 * num_2}')
+        elif sign == '-':
+            print(f'Результат: {num_1 - num_2}')
+        elif sign == '/':
+            print(f'Результат: {num_1 / num_2}')
+    except ZeroDivisionError as e:
+        print(e)
+        exit(errno.EINVAL)
+    except Exception as e:
+        print(e)
